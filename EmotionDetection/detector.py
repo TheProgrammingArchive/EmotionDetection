b39b07f27ba08a995628ead5a501a8c27ddc8089
TheProgrammingArchive <55741209+TheProgrammingArchive@users.noreply.github.com>
2022-12-31 12:12:04 +0530

    Add files via upload

--- EmotionDetection/detector.py
+++ EmotionDetection/detector.py
@@ -3,18 +3,27 @@ import numpy as np
 import cv2
 from keras.utils import img_to_array
 from keras.preprocessing import image
+from PIL import ImageGrab
+import sys
+
+arg = sys.argv[1]
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 prediction_model = load_model('model')
 age_model = load_model('amongus')
 
-capture = cv2.VideoCapture(0)
+if (arg == '-c'):
+    capture = cv2.VideoCapture(0)
 
 emotion_labels = {0:'Angry', 1:'Disgust', 2:'Fear', 3:'Happy', 4:'Neutral', 5:'Sad', 6:'Surprise'}
 colors = {0:(0, 128, 255), 1:(246, 61, 252), 2:(246, 61, 252), 3:(0,255,90), 4:(204, 204, 204), 5:(237, 28, 63), 6:(252,238,33)}
 
 while True:
-    _, frame = capture.read()
+    if (arg == '-c'):
+        _, frame = capture.read()
+    else:    
+        frame = np.array(ImageGrab.grab(bbox=(40,300,1200,1000)))
+        
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     face = face_cascade.detectMultiScale(gray, 1.3, 5)
 
