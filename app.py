1cd265c27cdcf6098a3477f659ce292a639fe628
TheProgrammingArchive <55741209+TheProgrammingArchive@users.noreply.github.com>
2023-08-04 21:14:54 +0530

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title('Emotion Detection and Age Recognition')
-st.subheader('Purav and Shrivatsan')
-st.divider()
-st.subheader('Emotion Detection')
-st.write('Our model uses Convolusional Neural Networks (CNNs) to identify emotions and classify them into 7 categories :gray[Happy] :gray[Sad] :gray[Angry] :gray[Neutral] :brown[Fear] :pink[Disgust] :turquoise[Surprise]')
-st.write()  
