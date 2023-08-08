f6822b3c2407de329fdeefcc8df4c107485e2033
smoRTy6626 <74461308+SyntaxTerror484@users.noreply.github.com>
2023-08-08 21:10:16 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -14,12 +14,22 @@ with col1:
     st.subheader('Emotion detection')
     st.write(''' Emotion detection is done using a classification model that categorizes emotions into 7 categories: Happy, Sad, Angry, Disgust, Fear, Neutral ''')
     st.write("The model uses 2 sets of CNN's with 2 units each and a connected layer to achieve a final accuracy of 68.5 on the test set")
-    st.image(Image.open("C:\\Users\\HP\\Desktop\\Screenshot 2023-08-04 214500.png"), caption="fard")
-
+    st.image(Image.open("images\\emotion.png"), caption="Emotion Detection model")
 with col2:
     st.subheader('Age detection')
-    st.write('Age detection is done using a regression model that predicts a persons ')
+    st.write('Age detection is done using a regression model that predicts a person\'s age in the range of 1-90')
+    st.write('The model uses 2 sets of CNN\'s with 2 units each and a complete regression layer')
 
-# st.image(Image.open("C:\\Users\\HP\\Desktop\\amongusre.png"), caption="fard")
 
-st.write()  
+
+    st.image(Image.open("images\\age.png"), caption="Age recognition model")
+
+
+st.subheader("Datasets used")
+st.write("Emotion Detection - FER13")
+st.write("Age Recognition - UTKFace")
+st.divider()
+
+st.subheader('Project Demo')
+st.write("The model can detect multiple faces and emotions")
+st.image(Image.open("images\\demo.png"), caption = 'an example of multiple face recognition')
